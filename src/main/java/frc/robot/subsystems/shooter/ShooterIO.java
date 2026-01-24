43e6f2b9b4ee4a74cc03385e9f2fe55904376b65
Doug Liu <dougdliu@gmail.com>
2026-01-24 00:27:25 -0700

    Added Hopper, Intake, and Shooter IO methods and IOInputs properties

--- src/main/java/frc/robot/subsystems/shooter/ShooterIO.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIO.java
@@ -1,5 +1,33 @@
 package frc.robot.subsystems.shooter;
 
-public class ShooterIO {
-    
+import lombok.Getter;
+import lombok.Setter;
+
+public interface ShooterIO {
+    default void setShooterMotorVoltage(double voltage) {
+        throw new UnsupportedOperationException("setShooterMotorVoltage is not implemented");
+    }
+
+    default void setShooterMotorPercentage(double percentage) {
+        throw new UnsupportedOperationException("setShooterMotorPercentage is not implemented");
+    }
+
+    default void setShooterMotorRPM(double rpm) {
+        throw new UnsupportedOperationException("setShooterMotorRPM is not implemented");
+    }
+
+    class ShooterIOInputs {
+
+        /** Are the shooter motors connected? */
+        @Getter @Setter private boolean shooterMotorConnected;
+
+        /** Shooter motor data */
+        @Getter @Setter private double shooterMotorVoltage;
+        @Getter @Setter private double shooterMotorDutyCycle;
+        @Getter @Setter private double shooterMotorCurrent;
+        @Getter @Setter private double shooterMotorTemperature;
+        @Getter @Setter private double shooterMotorVelocity;
+    }
+
+    default void updateInputs(ShooterIOInputs inputs) {}
 }
