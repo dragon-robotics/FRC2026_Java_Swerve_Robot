0080caaed2b7dae50bc038bb8420c4e2b022a677
Doug Liu <dougdliu@gmail.com>
2026-01-24 00:08:52 -0700

    Added base subsystem code and superstructure

---
+++ src/main/java/frc/robot/subsystems/hopper/HopperSubsystem.java
@@ -0,0 +1,17 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.hopper;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class HopperSubsystem extends SubsystemBase {
+  /** Creates a new HopperSubsystem. */
+  public HopperSubsystem() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
