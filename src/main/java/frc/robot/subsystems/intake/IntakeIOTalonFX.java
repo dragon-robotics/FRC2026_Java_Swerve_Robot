01e07ee0f0b12708dd3144da95b98800b7ecfd00
Doug Liu <dougdliu@gmail.com>
2026-02-07 03:53:18 -0700

    Simplified all motor IOs to an individual motor.

--- src/main/java/frc/robot/subsystems/intake/IntakeIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOTalonFX.java
@@ -1,178 +1,65 @@
 package frc.robot.subsystems.intake;
 
-import static edu.wpi.first.units.Units.Amps;
-import static edu.wpi.first.units.Units.Seconds;
-import static edu.wpi.first.units.Units.Volts;
 import static frc.robot.Constants.IntakeSubsystemConstants.*;
 
-import com.ctre.phoenix6.configs.ClosedLoopRampsConfigs;
-import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
-import com.ctre.phoenix6.configs.MotionMagicConfigs;
-import com.ctre.phoenix6.configs.MotorOutputConfigs;
-import com.ctre.phoenix6.configs.OpenLoopRampsConfigs;
-import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.configs.VoltageConfigs;
 import com.ctre.phoenix6.controls.MotionMagicExpoTorqueCurrentFOC;
 import com.ctre.phoenix6.controls.MotionMagicVelocityTorqueCurrentFOC;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 public class IntakeIOTalonFX implements IntakeIO {
 
-  private final TalonFX intakeRollerMotor;
-  private final TalonFX intakeArmMotor;
+  private final TalonFX motor;
 
-  private final MotionMagicVelocityTorqueCurrentFOC
-      rollerMotionMagicVelocityTorqueCurrentFOCRequest;
-  private final MotionMagicExpoTorqueCurrentFOC intakeArmMotionMagicExpoTorqueCurrentFOCRequest;
+  private final MotionMagicVelocityTorqueCurrentFOC motorMotionMagicVelocityTorqueCurrentFOCRequest;
+  private final MotionMagicExpoTorqueCurrentFOC motorMotionMagicExpoTorqueCurrentFOCRequest;
 
-  public IntakeIOTalonFX() {
+  public IntakeIOTalonFX(int canID, TalonFXConfiguration config) {
 
     /* Instantiate the motors */
-    intakeRollerMotor = new TalonFX(INTAKE_ROLLER_MOTOR_ID);
-    intakeArmMotor = new TalonFX(INTAKE_ARM_MOTOR_ID);
+    motor = new TalonFX(canID);
 
     /* Clear any existing faults */
-    intakeRollerMotor.clearStickyFaults();
-    intakeArmMotor.clearStickyFaults();
+    motor.clearStickyFaults();
 
     /* Configure the motors */
-    /* Roller motor configuration */
-    TalonFXConfiguration rollerConfig =
-        new TalonFXConfiguration()
-            .withCurrentLimits(
-                new CurrentLimitsConfigs()
-                    .withStatorCurrentLimitEnable(true)
-                    .withStatorCurrentLimit(Amps.of(INTAKE_ROLLER_STATOR_CURRENT_LIMIT))
-                    .withSupplyCurrentLimitEnable(true)
-                    .withSupplyCurrentLimit(Amps.of(INTAKE_ROLLER_SUPPLY_CURRENT_LIMIT)))
-            .withVoltage(
-                new VoltageConfigs()
-                    .withPeakForwardVoltage(Volts.of(INTAKE_ROLLER_MAX_VOLTAGE))
-                    .withPeakReverseVoltage(Volts.of(-INTAKE_ROLLER_MAX_VOLTAGE)))
-            .withOpenLoopRamps(
-                new OpenLoopRampsConfigs()
-                    .withDutyCycleOpenLoopRampPeriod(Seconds.of(INTAKE_ROLLER_RAMP_RATE))
-                    .withTorqueOpenLoopRampPeriod(Seconds.of(INTAKE_ROLLER_RAMP_RATE))
-                    .withVoltageOpenLoopRampPeriod(Seconds.of(INTAKE_ROLLER_RAMP_RATE)))
-            .withClosedLoopRamps(
-                new ClosedLoopRampsConfigs()
-                    .withDutyCycleClosedLoopRampPeriod(Seconds.of(INTAKE_ROLLER_RAMP_RATE))
-                    .withTorqueClosedLoopRampPeriod(Seconds.of(INTAKE_ROLLER_RAMP_RATE))
-                    .withVoltageClosedLoopRampPeriod(Seconds.of(INTAKE_ROLLER_RAMP_RATE)))
-            .withMotorOutput(new MotorOutputConfigs().withNeutralMode(INTAKE_ROLLER_NEUTRAL_MODE));
-
-    /* Roller motor PID configs for Velocity Control */
-    Slot0Configs rollerSlot0Configs = rollerConfig.Slot0; // @TODO: NEEDS TO BE TESTED AND TUNED
-    rollerSlot0Configs.kS = 0.25; // Add 0.25 V output to overcome static friction
-    rollerSlot0Configs.kV = 0.12; // A velocity target of 1 rps results in 0.12 V output
-    rollerSlot0Configs.kA = 0.01; // An acceleration of 1 rps/s requires 0.01 V output
-    rollerSlot0Configs.kP = 0.11; // An error of 1 rps results in 0.11 V output
-    rollerSlot0Configs.kI = 0; // no output for integrated error
-    rollerSlot0Configs.kD = 0; // no output for error derivative
-
-    // set Motion Magic Velocity settings
-    MotionMagicConfigs rollerMotionMagicConfigs = rollerConfig.MotionMagic;
-    rollerMotionMagicConfigs.MotionMagicAcceleration =
-        400; // Target acceleration of 400 rps/s (0.25 seconds to max)
-    rollerMotionMagicConfigs.MotionMagicJerk = 4000; // Target jerk of 4000 rps/s/s (0.1 seconds)
-
-    /* Arm motor configuration */
-    TalonFXConfiguration armConfig =
-        new TalonFXConfiguration()
-            .withCurrentLimits(
-                new CurrentLimitsConfigs()
-                    .withStatorCurrentLimitEnable(true)
-                    .withStatorCurrentLimit(Amps.of(INTAKE_ARM_STATOR_CURRENT_LIMIT))
-                    .withSupplyCurrentLimitEnable(true)
-                    .withSupplyCurrentLimit(Amps.of(INTAKE_ARM_SUPPLY_CURRENT_LIMIT)))
-            .withVoltage(
-                new VoltageConfigs()
-                    .withPeakForwardVoltage(Volts.of(INTAKE_ARM_MAX_VOLTAGE))
-                    .withPeakReverseVoltage(Volts.of(-INTAKE_ARM_MAX_VOLTAGE)))
-            .withOpenLoopRamps(
-                new OpenLoopRampsConfigs()
-                    .withDutyCycleOpenLoopRampPeriod(Seconds.of(INTAKE_ARM_RAMP_RATE))
-                    .withTorqueOpenLoopRampPeriod(Seconds.of(INTAKE_ARM_RAMP_RATE))
-                    .withVoltageOpenLoopRampPeriod(Seconds.of(INTAKE_ARM_RAMP_RATE)))
-            .withClosedLoopRamps(
-                new ClosedLoopRampsConfigs()
-                    .withDutyCycleClosedLoopRampPeriod(Seconds.of(INTAKE_ARM_RAMP_RATE))
-                    .withTorqueClosedLoopRampPeriod(Seconds.of(INTAKE_ARM_RAMP_RATE))
-                    .withVoltageClosedLoopRampPeriod(Seconds.of(INTAKE_ARM_RAMP_RATE)))
-            .withMotorOutput(new MotorOutputConfigs().withNeutralMode(INTAKE_ARM_NEUTRAL_MODE));
-
-    /* Arm motor PID configs for Position Control */
-    Slot0Configs armSlot0Configs = armConfig.Slot0; // @TODO: NEEDS TO BE TESTED AND TUNED
-    armSlot0Configs.kS = 0.25; // Add 0.25 V output to overcome static friction
-    armSlot0Configs.kV = 0.12; // A velocity target of 1 rps results in 0.12 V output
-    armSlot0Configs.kA = 0.01; // An acceleration of 1 rps/s requires 0.01 V output
-    armSlot0Configs.kP = 4.8; // A position error of 2.5 rotations results in 12 V output
-    armSlot0Configs.kI = 0; // no output for integrated error
-    armSlot0Configs.kD = 0.1; // A velocity error of 1 rps results in 0.1 V output
-
-    // set Motion Magic Expo settings
-    var armMotionMagicConfigs = armConfig.MotionMagic;
-    armMotionMagicConfigs.MotionMagicCruiseVelocity = 0; // Unlimited cruise velocity
-    armMotionMagicConfigs.MotionMagicExpo_kV = 0.12; // kV is around 0.12 V/rps
-    armMotionMagicConfigs.MotionMagicExpo_kA = 0.1; // Use a slower kA of 0.1 V/(rps/s)
-
-    intakeRollerMotor.getConfigurator().apply(rollerConfig);
-    intakeArmMotor.getConfigurator().apply(armConfig);
+    motor.getConfigurator().apply(config);
 
     /* Create Motion Magic Velocity and Motion Magic Expo requests */
-    rollerMotionMagicVelocityTorqueCurrentFOCRequest = new MotionMagicVelocityTorqueCurrentFOC(0);
-    intakeArmMotionMagicExpoTorqueCurrentFOCRequest = new MotionMagicExpoTorqueCurrentFOC(0);
+    motorMotionMagicVelocityTorqueCurrentFOCRequest = new MotionMagicVelocityTorqueCurrentFOC(0);
+    motorMotionMagicExpoTorqueCurrentFOCRequest = new MotionMagicExpoTorqueCurrentFOC(0);
   }
 
   @Override
-  public void setIntakeRollerMotorVoltage(double voltage) {
-    intakeRollerMotor.setVoltage(voltage);
+  public void setMotorVoltage(double voltage) {
+    motor.setVoltage(voltage);
   }
 
   @Override
-  public void setIntakeRollerMotorPercentage(double percentage) {
-    intakeRollerMotor.set(percentage);
+  public void setMotorPercentage(double percentage) {
+    motor.set(percentage);
   }
 
   @Override
-  public void setIntakeRollerMotorRPM(double rpm) {
-    intakeRollerMotor.setControl(
-        rollerMotionMagicVelocityTorqueCurrentFOCRequest.withVelocity(rpm / 60));
+  public void setMotorRPM(double rpm) {
+    motor.setControl(motorMotionMagicVelocityTorqueCurrentFOCRequest.withVelocity(rpm / 60));
   }
 
   @Override
-  public void setIntakeArmMotorVoltage(double voltage) {
-    intakeArmMotor.setVoltage(voltage);
-  }
-
-  @Override
-  public void setIntakeArmMotorPercentage(double percentage) {
-    intakeArmMotor.set(percentage);
-  }
-
-  @Override
-  public void setIntakeArmMotorSetpoint(double setpoint) {
-    intakeArmMotor.setControl(
-        intakeArmMotionMagicExpoTorqueCurrentFOCRequest.withPosition(setpoint));
+  public void setMotorPosition(double setpoint) {
+    motor.setControl(motorMotionMagicExpoTorqueCurrentFOCRequest.withPosition(setpoint));
   }
 
   @Override
   public void updateInputs(IntakeIOInputs inputs) {
 
-    inputs.setIntakeRollerMotorConnected(intakeRollerMotor.isConnected());
-    inputs.setIntakeArmMotorConnected(intakeArmMotor.isConnected());
+    inputs.setMotorConnected(motor.isConnected());
 
-    inputs.setIntakeRollerMotorVoltage(intakeRollerMotor.getMotorVoltage().getValueAsDouble());
-    inputs.setIntakeRollerMotorDutyCycle(intakeRollerMotor.getDutyCycle().getValueAsDouble());
-    inputs.setIntakeRollerMotorCurrent(intakeRollerMotor.getStatorCurrent().getValueAsDouble());
-    inputs.setIntakeRollerMotorTemperature(intakeRollerMotor.getDeviceTemp().getValueAsDouble());
-    inputs.setIntakeRollerMotorVelocity(intakeRollerMotor.getVelocity().getValueAsDouble());
-
-    inputs.setIntakeArmMotorVoltage(intakeArmMotor.getMotorVoltage().getValueAsDouble());
-    inputs.setIntakeArmMotorDutyCycle(intakeArmMotor.getDutyCycle().getValueAsDouble());
-    inputs.setIntakeArmMotorCurrent(intakeArmMotor.getStatorCurrent().getValueAsDouble());
-    inputs.setIntakeArmMotorTemperature(intakeArmMotor.getDeviceTemp().getValueAsDouble());
-    inputs.setIntakeArmMotorSetpoint(intakeArmMotor.getPosition().getValueAsDouble());
+    inputs.setMotorVoltage(motor.getMotorVoltage().getValueAsDouble());
+    inputs.setMotorDutyCycle(motor.getDutyCycle().getValueAsDouble());
+    inputs.setMotorCurrent(motor.getStatorCurrent().getValueAsDouble());
+    inputs.setMotorTemperature(motor.getDeviceTemp().getValueAsDouble());
+    inputs.setMotorVelocity(motor.getVelocity().getValueAsDouble());
+    inputs.setMotorPosition(motor.getPosition().getValueAsDouble());
   }
 }
