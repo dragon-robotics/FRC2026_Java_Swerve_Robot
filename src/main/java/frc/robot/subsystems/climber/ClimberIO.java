1ffb4025078c1f9da7fa6da7dbf560ac356a2407
da4nk <alexmn781@gmail.com>
2026-02-07 13:11:38 -0700

    Implemented Climber subsystem

--- src/main/java/frc/robot/subsystems/climber/ClimberIO.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberIO.java
@@ -1,36 +1,48 @@
 package frc.robot.subsystems.climber;
-
 import lombok.Getter;
 import lombok.Setter;
 
 public interface ClimberIO {
+    
+    default void setElevatorSetpoint(double setPoint) {
+        throw new UnsupportedOperationException("setElevatorSetpoint not implemented");
+    }
+    /* getters */
+    default boolean isElevatorStowed(double homeSetPoint, double tolerance) {
+        throw new UnsupportedOperationException("isElevatorStowed not implemented");
+    }
 
-  default void setClimberMotorVoltage(double voltage) {
-    throw new UnsupportedOperationException("setClimberMotorVoltage is not implemented");
-  }
+    default boolean hasClimbed() {
+        throw new UnsupportedOperationException("hasClimbed not implemented");
+    }
 
-  default void setClimberMotorPercentage(double percentage) {
-    throw new UnsupportedOperationException("setClimberMotorPercentage is not implemented");
-  }
+    default boolean getCurrentSpike() {
+        throw new UnsupportedOperationException("getCurrentLimit not implemented");
+    }
 
-  default void setClimberSetpoint(double setpoint) {
-    throw new UnsupportedOperationException("setClimberSetpoint is not implemented");
-  }
+    default double getElevatorPosition() {
+        throw new UnsupportedOperationException("getElevatorPosition not implemented");
+    }
 
-  class ClimberIOInputs {
+    class ClimberIOInputs {
+        // Motor connection status
+        @Getter @Setter private boolean leadMotorConnected;
+        @Getter @Setter private boolean followerMotorConnected;
+        
+        // Lead motor data
+        @Getter @Setter private double leadMotorPosition;
+        @Getter @Setter private double leadMotorVelocity;
+        @Getter @Setter private double leadMotorVoltage;
+        @Getter @Setter private double leadMotorCurrent;
+        @Getter @Setter private double leadMotorTemperature;
+        
+        // Follower motor data
+        @Getter @Setter private double followerMotorPosition;
+        @Getter @Setter private double followerMotorVelocity;
+        @Getter @Setter private double followerMotorVoltage;
+        @Getter @Setter private double followerMotorCurrent;
+        @Getter @Setter private double followerMotorTemperature;
+    }
 
-    /** Are the climber motors connected? */
-    @Getter @Setter private boolean climberMotorConnected;
-
-    /** Climber motor data */
-    @Getter @Setter private double climberMotorVoltage;
-
-    @Getter @Setter private double climberMotorDutyCycle;
-    @Getter @Setter private double climberMotorCurrent;
-    @Getter @Setter private double climberMotorTemperature;
-    @Getter @Setter private double climberMotorSetpoint;
-    @Getter @Setter private double climberMotorVelocity;
-  }
-
-  default void updateInputs(ClimberIOInputs inputs) {}
+    default void updateInputs(ClimberIOInputs climberInputs) {}
 }
--- src/main/java/frc/robot/subsystems/climber/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberSubsystem.java
@@ -1,17 +1,96 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems.climber;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.climber.ClimberIO.ClimberIOInputs;
+import static frc.robot.Constants.ClimberConstants.*;
 
-public class ClimberSubsystem extends SubsystemBase {
-  /** Creates a new ClimberSubsystem. */
-  public ClimberSubsystem() {}
+public class ClimberSubsystem extends SubsystemBase{
+    
+    public enum ClimberState {
+        STOWED, 
+        L1,
+        TRANSISTION_L1,
+        STOWING,
+    }
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
+    private ClimberState currentClimberState;
+    private ClimberState desiredClimberState;
+
+    private ClimberIO climberIO;
+    private ClimberIOInputs climberInputs; 
+
+    public ClimberSubsystem(ClimberIO climberIO) {
+        this.climberIO = climberIO;
+        this.climberInputs = new ClimberIOInputs();
+
+        // initailize climber states
+        this.desiredClimberState = ClimberState.STOWED;
+        this.currentClimberState = ClimberState.STOWED;
+    }
+
+    public void setDesiredState(ClimberState state) {
+        this.desiredClimberState = state;
+        
+        switch (desiredClimberState) {
+            case STOWED: 
+                currentClimberState = ClimberState.STOWING;
+                break;
+            case L1: 
+                currentClimberState = ClimberState.TRANSISTION_L1;
+                break;
+            default:
+                break;
+        } 
+    }
+
+    public void setElevatorSetpoint(double setPoint) {
+        climberIO.setElevatorSetpoint(setPoint);
   }
+    /* getters */
+    public ClimberState getCurrentState() {
+        return currentClimberState;
+    }
+
+    public ClimberState getDesiredState() {
+        return desiredClimberState;
+    }
+    public double getElevatorPosition() {
+        return climberIO.getElevatorPosition();
+    }
+
+    private boolean isAtPosition(double setPoint, double tolerance) {
+        return Math.abs(getElevatorPosition() - setPoint) < tolerance;
+    }
+
+    public boolean hasClimbed(double setPoint, double tolerance) {
+        return Math.abs(setPoint - getElevatorPosition()) < tolerance;
+    }
+    public boolean isHome(double homeSetPoint, double tolerance) {
+        return climberIO.isElevatorStowed(homeSetPoint, tolerance);
+    }
+    @Override
+    public void periodic() {
+        climberIO.updateInputs(climberInputs);
+        switch (currentClimberState) {
+            case STOWED: 
+                break;
+            case STOWING:
+                setElevatorSetpoint(CLIMBER_RETRACTED_SETPOINT);
+                if (isAtPosition(CLIMBER_RETRACTED_SETPOINT, POSITION_TOLERANCE)) {
+                    currentClimberState = ClimberState.STOWED;
+                }
+                break;
+            case TRANSISTION_L1:
+               setElevatorSetpoint(CLIMBER_L1_SETPOINT);
+                if (isAtPosition(CLIMBER_HOME_SETPOINT, POSITION_TOLERANCE)) {
+                    currentClimberState = ClimberState.L1;
+                }
+                break;
+            case L1:
+                if (desiredClimberState == ClimberState.STOWED) {
+                    currentClimberState = ClimberState.STOWING;
+                }
+                break;
+        }
+    }
 }
+
