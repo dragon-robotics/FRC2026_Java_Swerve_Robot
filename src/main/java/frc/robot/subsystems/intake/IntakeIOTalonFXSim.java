edc66fe683e4cd2d862118909a02c7893ce774f5
Doug Liu <dougdliu@gmail.com>
2026-02-09 01:14:09 -0700

    Added intake arm sim stubs

---
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOTalonFXSim.java
@@ -0,0 +1,10 @@
+package frc.robot.subsystems.intake;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+
+public class IntakeIOTalonFXSim extends IntakeIOTalonFX {
+    
+  public IntakeIOTalonFXSim(int canID, TalonFXConfiguration config) {
+    super(canID, config);
+  }
+}
