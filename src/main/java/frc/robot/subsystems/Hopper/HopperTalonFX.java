7eb1f85f0bfc3512506f183a7779f6c5fd9330b8
Doug Liu <dougdliu@gmail.com>
2026-02-14 04:12:43 -0700

    Linting changes

--- src/main/java/frc/robot/subsystems/Hopper/HopperTalonFX.java
+++ src/main/java/frc/robot/subsystems/Hopper/HopperTalonFX.java
@@ -4,53 +4,48 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.MotionMagicExpoTorqueCurrentFOC;
 import com.ctre.phoenix6.controls.MotionMagicVelocityTorqueCurrentFOC;
 import com.ctre.phoenix6.hardware.TalonFX;
+
 public class HopperTalonFX implements HopperIO {
-    
-    private final TalonFX hopperMotor;
 
-    private final MotionMagicVelocityTorqueCurrentFOC
-        rollerMotionMagicVelocityTorqueCurrentFOC;
-    private final MotionMagicExpoTorqueCurrentFOC 
-        expandingMotorMotionMagicTorqueCurrentFOC;
+  private final TalonFX hopperMotor;
 
-    public HopperTalonFX (int canID, TalonFXConfiguration config) {
-        hopperMotor = new TalonFX(canID);
-        hopperMotor.getConfigurator().apply(config);
+  private final MotionMagicVelocityTorqueCurrentFOC rollerMotionMagicVelocityTorqueCurrentFOC;
+  private final MotionMagicExpoTorqueCurrentFOC expandingMotorMotionMagicTorqueCurrentFOC;
 
-        rollerMotionMagicVelocityTorqueCurrentFOC = new MotionMagicVelocityTorqueCurrentFOC(0);
-        expandingMotorMotionMagicTorqueCurrentFOC = new MotionMagicExpoTorqueCurrentFOC (0);
-    }
+  public HopperTalonFX(int canID, TalonFXConfiguration config) {
+    hopperMotor = new TalonFX(canID);
+    hopperMotor.getConfigurator().apply(config);
 
-    @Override 
-    public void expandHopper(double setpoint) {
-        hopperMotor.setControl(
-            expandingMotorMotionMagicTorqueCurrentFOC.withPosition(setpoint)
-        );
-     
-    }
-    @Override
-    public void runRollers(double rpm) {
-        hopperMotor.setControl(
-            rollerMotionMagicVelocityTorqueCurrentFOC.withVelocity(rpm)
-        );
-    }
+    rollerMotionMagicVelocityTorqueCurrentFOC = new MotionMagicVelocityTorqueCurrentFOC(0);
+    expandingMotorMotionMagicTorqueCurrentFOC = new MotionMagicExpoTorqueCurrentFOC(0);
+  }
 
-    @Override
-    public void stowHopper() {
-        expandHopper(0.0);
-        runRollers(0.0);
-    }
+  @Override
+  public void expandHopper(double setpoint) {
+    hopperMotor.setControl(expandingMotorMotionMagicTorqueCurrentFOC.withPosition(setpoint));
+  }
 
-    @Override
-    public void updateInputs(HopperIOInputs hopperInputs) {
-        // Motor connection status
-        hopperInputs.setExpandingMotorConnected(hopperMotor.isConnected());
-        
-        // Expanding motor data
-        hopperInputs.setExpandingMotorPosition(hopperMotor.getPosition().getValueAsDouble());
-        hopperInputs.setExpandingMotorVelocity(hopperMotor.getVelocity().getValueAsDouble());
-        hopperInputs.setExpandingMotorVoltage(hopperMotor.getMotorVoltage().getValueAsDouble());
-        hopperInputs.setExpandingMotorTemprature(hopperMotor.getDeviceTemp().getValueAsDouble());
-        hopperInputs.setExpandingMotorCurrent(hopperMotor.getStatorCurrent().getValueAsDouble());       
-    }
+  @Override
+  public void runRollers(double rpm) {
+    hopperMotor.setControl(rollerMotionMagicVelocityTorqueCurrentFOC.withVelocity(rpm));
+  }
+
+  @Override
+  public void stowHopper() {
+    expandHopper(0.0);
+    runRollers(0.0);
+  }
+
+  @Override
+  public void updateInputs(HopperIOInputs hopperInputs) {
+    // Motor connection status
+    hopperInputs.setExpandingMotorConnected(hopperMotor.isConnected());
+
+    // Expanding motor data
+    hopperInputs.setExpandingMotorPosition(hopperMotor.getPosition().getValueAsDouble());
+    hopperInputs.setExpandingMotorVelocity(hopperMotor.getVelocity().getValueAsDouble());
+    hopperInputs.setExpandingMotorVoltage(hopperMotor.getMotorVoltage().getValueAsDouble());
+    hopperInputs.setExpandingMotorTemprature(hopperMotor.getDeviceTemp().getValueAsDouble());
+    hopperInputs.setExpandingMotorCurrent(hopperMotor.getStatorCurrent().getValueAsDouble());
+  }
 }
--- src/main/java/frc/robot/subsystems/hopper/HopperIO.java
+++ src/main/java/frc/robot/subsystems/hopper/HopperIO.java
@@ -1,42 +1,46 @@
 package frc.robot.subsystems.hopper;
+
 import frc.robot.subsystems.hopper.HopperIO.HopperIOInputs;
 import lombok.Getter;
 import lombok.Setter;
 
 public interface HopperIO {
 
-    default void expandHopper(double setpoint) {
-        throw new UnsupportedOperationException("expandHopper not implemented");
-    }
-    default void stowHopper() {
-        throw new UnsupportedOperationException("stowHopper not implemented");
-    }
-    /* getters */
-    default void getCurrent() {
-        throw new UnsupportedOperationException("getCurrent not implemented");
-    }
-    
-    default void runRollers(double rpm) {
-        throw new UnsupportedOperationException("getCurrent not implemented");
-    }
+  default void expandHopper(double setpoint) {
+    throw new UnsupportedOperationException("expandHopper not implemented");
+  }
 
-    public class HopperIOInputs {
-        // are the motors connected 
-        @Getter @Setter private boolean expandingMotorConnected; 
+  default void stowHopper() {
+    throw new UnsupportedOperationException("stowHopper not implemented");
+  }
 
-        @Getter @Setter private boolean rollingMotorConnected; 
-        // expanding motor data
-        @Getter @Setter private double expandingMotorPosition; 
-        @Getter @Setter private double expandingMotorVelocity; 
-        @Getter @Setter private double expandingMotorVoltage; 
-        @Getter @Setter private double expandingMotorTemprature;
-        @Getter @Setter private double expandingMotorCurrent;
-        
-        // hopper roller motor data
-        @Getter @Setter private double rollerMotorVelocity;
-        @Getter @Setter private double rollerMotorVoltage; 
-        @Getter @Setter private double rollerMotorCurrent; 
-        @Getter @Setter private double rollerMotorTemprature;
-    }
-    default void updateInputs(HopperIOInputs hopperInputs) {}
+  /* getters */
+  default void getCurrent() {
+    throw new UnsupportedOperationException("getCurrent not implemented");
+  }
+
+  default void runRollers(double rpm) {
+    throw new UnsupportedOperationException("getCurrent not implemented");
+  }
+
+  public class HopperIOInputs {
+    // are the motors connected
+    @Getter @Setter private boolean expandingMotorConnected;
+
+    @Getter @Setter private boolean rollingMotorConnected;
+    // expanding motor data
+    @Getter @Setter private double expandingMotorPosition;
+    @Getter @Setter private double expandingMotorVelocity;
+    @Getter @Setter private double expandingMotorVoltage;
+    @Getter @Setter private double expandingMotorTemprature;
+    @Getter @Setter private double expandingMotorCurrent;
+
+    // hopper roller motor data
+    @Getter @Setter private double rollerMotorVelocity;
+    @Getter @Setter private double rollerMotorVoltage;
+    @Getter @Setter private double rollerMotorCurrent;
+    @Getter @Setter private double rollerMotorTemprature;
+  }
+
+  default void updateInputs(HopperIOInputs hopperInputs) {}
 }
