e1a0d30e95eb260f0c4b7e76169893d4d21631ca
da4nk <alexmn781@gmail.com>
2026-02-14 15:00:53 -0700

    Refactored Climber Subystem and implemented Spark Max implementation

---
+++ src/main/java/frc/robot/subsystems/climber/ClimberIOSparkMax.java
@@ -0,0 +1,90 @@
+package frc.robot.subsystems.climber;
+
+import java.util.Optional;
+
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.config.AbsoluteEncoderConfig;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+public class ClimberIOSparkMax implements ClimberIO {
+    protected final SparkMax motor;
+    protected final int canID;
+    protected final SparkMaxConfig config;
+    protected final String motorType;
+
+    private final SparkClosedLoopController motorController;
+    private final Optional<AbsoluteEncoderConfig> absEncoderConfig;
+
+    public ClimberIOSparkMax(int canID, SparkMaxConfig config, String motorType) {
+        this(canID, config, motorType, Optional.empty());
+    }
+
+    public ClimberIOSparkMax(
+            int canID,
+            SparkMaxConfig config,
+            String motorType,
+            Optional<AbsoluteEncoderConfig> absEncoderConfig) {
+        this.canID = canID;
+        this.config = config;
+        this.motorType = motorType;
+        this.absEncoderConfig = absEncoderConfig;
+
+        // Instantiate the motors and encoders//
+        motor = new SparkMax(this.canID, MotorType.kBrushless);
+        motorController = motor.getClosedLoopController();
+
+        // Clear any existing faults//
+        motor.clearFaults();
+
+        this.absEncoderConfig.ifPresentOrElse(
+                absEncCfg -> {
+                    this.config.apply(absEncCfg);
+                    motor.configure(
+                            this.config, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
+                    motor.getEncoder().setPosition(0);
+                }, () -> {
+                    motor.configure(
+                            this.config, ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);
+                    motor.getEncoder().setPosition(0);
+                });
+    }
+
+    @Override
+    public void setMotorVoltage(double voltage) {
+        motor.setVoltage(voltage);
+    }
+
+    @Override
+    public void setMotorPercentage(double percentage) {
+        motor.set(percentage);
+    }
+
+    @Override
+    public void setMotorRPM(double rpm) {
+        motorController.setSetpoint(rpm, ControlType.kMAXMotionVelocityControl);
+    }
+
+    @Override
+    public void setMotorPosition(double position) {
+        motorController.setSetpoint(position, ControlType.kMAXMotionPositionControl);
+    }
+
+    @Override
+    public void updateInputs(ClimberIOInputs climberInputs) {
+        // Motor connection status
+        climberInputs.setLeadMotorConnected(motor.getDeviceId() == canID);
+
+        // Lead motor data
+        climberInputs.setLeadMotorPosition(motor.getEncoder().getPosition());
+        climberInputs.setLeadMotorVelocity(motor.getEncoder().getVelocity());
+        climberInputs.setLeadMotorVoltage(motor.getAppliedOutput() * motor.getBusVoltage());
+        climberInputs.setLeadMotorCurrent(motor.getOutputCurrent());
+        climberInputs.setLeadMotorTemperature(motor.getMotorTemperature());
+    }
+
+}
--- src/main/java/frc/robot/subsystems/climber/ClimberIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberIOTalonFX.java
@@ -1,65 +1,106 @@
 package frc.robot.subsystems.climber;
 
+import static frc.robot.Constants.ClimberConstants.CLIMBER_GEAR_RATIO;
+
+import java.util.Optional;
+
+import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.configs.FeedbackConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.MotionMagicExpoTorqueCurrentFOC;
+import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.MotorAlignmentValue;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
 
 public class ClimberIOTalonFX implements ClimberIO {
 
-  private final TalonFX leadMotor;
-  private final TalonFX followerMotor;
-  private final MotionMagicExpoTorqueCurrentFOC climberMotionMagicTorqueCurrentFOC;
+  protected final TalonFX motor;
+  protected final int canID;
+  protected final TalonFXConfiguration config;
+  private final Optional<CANcoderConfiguration> canCoderConfig;
+  private final MotionMagicExpoTorqueCurrentFOC motorMotionMagicTorqueCurrentFOC;
 
   public ClimberIOTalonFX(int canID, TalonFXConfiguration config) {
-    leadMotor = new TalonFX(canID);
-    followerMotor = new TalonFX(canID);
-    // Apply configurations to lead motor
-    leadMotor.getConfigurator().apply(config);
-    followerMotor.getConfigurator().apply(config);
+    this.canID = canID;
+    this.config = config;
+    this.canCoderConfig = Optional.empty();
 
-    // Set follower to follow lead motor
-    followerMotor.setControl(new Follower(leadMotor.getDeviceID(), MotorAlignmentValue.Aligned));
+    this.motor = new TalonFX(this.canID, CANBus.roboRIO());
+    this.motor.clearStickyFaults();
+    this.motorMotionMagicTorqueCurrentFOC = new MotionMagicExpoTorqueCurrentFOC(0);
+  }
 
-    // Create control requests
-    climberMotionMagicTorqueCurrentFOC = new MotionMagicExpoTorqueCurrentFOC(0.0);
+  public ClimberIOTalonFX(int canID, TalonFXConfiguration config, int leadCanID,
+      Optional<CANcoderConfiguration> canCoderConfig) {
+    this.canID = canID;
+    this.config = config;
+    this.canCoderConfig = canCoderConfig;
+
+    motor = new TalonFX(this.canID, CANBus.roboRIO());
+    motor.clearStickyFaults();
+
+    motorMotionMagicTorqueCurrentFOC = new MotionMagicExpoTorqueCurrentFOC(0);
+    this.canCoderConfig.ifPresentOrElse(
+        ccCfg -> {
+          try (CANcoder canCoder = new CANcoder(1, CANBus.roboRIO())) {
+            canCoder.getConfigurator().apply(ccCfg);
+            TalonFXConfiguration cfg = this.config.withFeedback(
+                new FeedbackConfigs()
+                    // .withFusedCANcoder(canCoder)
+                    .withFeedbackSensorSource(FeedbackSensorSourceValue.RemoteCANcoder)
+                    .withRotorToSensorRatio(CLIMBER_GEAR_RATIO)
+                    .withSensorToMechanismRatio(1)
+                    .withFeedbackRotorOffset(1)
+                    .withFeedbackRotorOffset(0));
+            motor.getConfigurator().apply(cfg);
+          } catch (Exception e) {
+            // Handle exceptions related to CANcoder configuration
+            System.err.println("Error configuring CANcoder: " + e.getMessage());
+          }
+        },
+        () -> {
+          // Handle the case where canCoderConfig is not present (e.g. set up feedback
+          // configs
+          // without CANcoder)
+          motor.getConfigurator().apply(this.config);
+          motor.setPosition(0);
+        });
   }
 
   @Override
-  public void setElevatorSetpoint(double setPoint) {
-    leadMotor.setControl(climberMotionMagicTorqueCurrentFOC.withPosition(setPoint));
-  }
-
-  /* getters */
-  @Override
-  public double getElevatorPosition() {
-    return leadMotor.getPosition().getValueAsDouble();
+  public void setMotorVoltage(double voltage) {
+    motor.setVoltage(voltage);
   }
 
   @Override
-  public boolean isElevatorStowed(double homeSetPoint, double tolerance) {
-    return Math.abs(getElevatorPosition() - homeSetPoint) < tolerance;
+  public void setMotorPercentage(double percentage) {
+    motor.set(percentage);
+  }
+
+  @Override
+  public void setMotorPosition(double setPoint) {
+    motor.setControl(motorMotionMagicTorqueCurrentFOC.withPosition(setPoint));
   }
 
   @Override
   public void updateInputs(ClimberIOInputs climberInputs) {
     // Motor connection status
-    climberInputs.setLeadMotorConnected(leadMotor.isConnected());
-    climberInputs.setFollowerMotorConnected(followerMotor.isConnected());
+    climberInputs.setLeadMotorConnected(motor.isConnected());
+    climberInputs.setFollowerMotorConnected(motor.isConnected());
 
     // Lead motor data
-    climberInputs.setLeadMotorPosition(leadMotor.getPosition().getValueAsDouble());
-    climberInputs.setLeadMotorVelocity(leadMotor.getVelocity().getValueAsDouble());
-    climberInputs.setLeadMotorVoltage(leadMotor.getMotorVoltage().getValueAsDouble());
-    climberInputs.setLeadMotorCurrent(leadMotor.getStatorCurrent().getValueAsDouble());
-    climberInputs.setLeadMotorTemperature(leadMotor.getDeviceTemp().getValueAsDouble());
+    climberInputs.setLeadMotorPosition(motor.getPosition().getValueAsDouble());
+    climberInputs.setLeadMotorVelocity(motor.getVelocity().getValueAsDouble());
+    climberInputs.setLeadMotorVoltage(motor.getMotorVoltage().getValueAsDouble());
+    climberInputs.setLeadMotorCurrent(motor.getStatorCurrent().getValueAsDouble());
+    climberInputs.setLeadMotorTemperature(motor.getDeviceTemp().getValueAsDouble());
 
     // Follower motor data
-    climberInputs.setFollowerMotorPosition(followerMotor.getPosition().getValueAsDouble());
-    climberInputs.setFollowerMotorVelocity(followerMotor.getVelocity().getValueAsDouble());
-    climberInputs.setFollowerMotorVoltage(followerMotor.getMotorVoltage().getValueAsDouble());
-    climberInputs.setFollowerMotorCurrent(followerMotor.getStatorCurrent().getValueAsDouble());
-    climberInputs.setFollowerMotorTemperature(followerMotor.getDeviceTemp().getValueAsDouble());
+    climberInputs.setFollowerMotorPosition(motor.getPosition().getValueAsDouble());
+    climberInputs.setFollowerMotorVelocity(motor.getVelocity().getValueAsDouble());
+    climberInputs.setFollowerMotorVoltage(motor.getMotorVoltage().getValueAsDouble());
+    climberInputs.setFollowerMotorCurrent(motor.getStatorCurrent().getValueAsDouble());
+    climberInputs.setFollowerMotorTemperature(motor.getDeviceTemp().getValueAsDouble());
   }
 }
