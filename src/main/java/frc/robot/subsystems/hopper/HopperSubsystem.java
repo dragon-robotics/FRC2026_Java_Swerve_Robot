05c485dd36aee199d678ceb4e679150abcb20cc5
cpc22 <cesar.parra0722@gmail.com>
2026-02-16 20:22:25 -0700

    refactored HopperSubsystem file for new subsystem design. added sim file

--- src/main/java/frc/robot/subsystems/hopper/HopperSubsystem.java
+++ src/main/java/frc/robot/subsystems/hopper/HopperSubsystem.java
@@ -3,13 +3,13 @@ package frc.robot.subsystems.hopper;
 import static frc.robot.Constants.HopperConstants.*;
 
 import dev.doglog.DogLog;
-import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.hopper.HopperIO.HopperIOInputs;
 
 public class HopperSubsystem extends SubsystemBase {
 
   public enum HopperState {
+    STOP,
     INDEXTOSHOOTER,
     INDEXTOINTAKE
   }
@@ -24,37 +24,33 @@ public class HopperSubsystem extends SubsystemBase {
   // hardware layer
   private final HopperIOInputs rollerMotorIOInputs;
 
+  /* Creates new HopperSubsystem */
   public HopperSubsystem(HopperIO rollerMotorIO) {
 
-    this.expandingMotorIO = expandingMotorIO;
     this.rollerMotorIO = rollerMotorIO;
-    this.expandingMotorInputs = new HopperIOInputs();
     this.rollerMotorIOInputs = new HopperIOInputs();
 
-    this.currHopperState = HopperState.STOWED;
-    this.desiredHopperState = HopperState.STOWED;
+    this.currHopperState = HopperState.STOP;
+    this.desiredHopperState = HopperState.STOP;
   }
+
+  /* Setters */
 
   public void setDesiredState(HopperState state) {
     this.desiredHopperState = state;
 
     switch (desiredHopperState) {
-      case STOWED:
-        currHopperState = HopperState.STOWING;
+      case STOP:
+        currHopperState = HopperState.STOP;
         break;
       case INDEXTOINTAKE:
         if (currHopperState == HopperState.INDEXTOSHOOTER) {
           currHopperState = HopperState.INDEXTOINTAKE;
-        } else {
-          // retracts hopper as we outtake fuel
-          currHopperState = HopperState.STOWING;
         }
         break;
       case INDEXTOSHOOTER:
         if (currHopperState == HopperState.INDEXTOINTAKE) {
           currHopperState = HopperState.INDEXTOSHOOTER;
-        } else {
-          currHopperState = HopperState.DEPLOYING;
         }
         break;
       default:
@@ -62,30 +58,7 @@ public class HopperSubsystem extends SubsystemBase {
     }
   }
 
-  /** Check if expanding motor has reached target position */
-  private boolean isDeployed(double targetPosition, double tolerance) {
-    return Math.abs(expandingMotorInputs.getExpandingMotorPosition() - targetPosition) < tolerance;
-  }
-
-  public void expandHopper() {
-    expandingMotorIO.expandHopper(EXPANDING_SETPOINT);
-  }
-
-  public void stowHopper() {
-    expandingMotorIO.stowHopper();
-  }
-
-  /** Index fuel toward shooter */
-  public void indexToShooter() {
-    rollerMotorIO.expandHopper(ROLLER_RPM);
-  }
-
-  /** Index fuel toward intake/outtake */
-  public void indexToIntake() {
-    rollerMotorIO.expandHopper(-ROLLER_RPM);
-  }
-
-  /* getters */
+  /* Getters */
 
   public HopperState getCurrentState() {
     return currHopperState;
@@ -95,47 +68,49 @@ public class HopperSubsystem extends SubsystemBase {
     return desiredHopperState;
   }
 
-  @Override
-  public void periodic() {
-    // Update sensor inputs
-    rollerMotorIO.updateInputs(rollerMotorIOInputs);
-    expandingMotorIO.updateInputs(expandingMotorInputs);
+  /* Functions */
 
-    // State machine logic
+  public void indexToShooter() {
+    rollerMotorIO.setMotorRPM(ROLLER_RPM);
+  }
+
+  public void indexToIntake() {
+    rollerMotorIO.setMotorRPM(-ROLLER_RPM);
+  }
+
+  public void stopIntake() {
+    rollerMotorIO.setMotorRPM(0);
+  }
+
+  public void handleStateTransition() {
+    
+    /* Handle the state transitions */
     switch (currHopperState) {
-      case STOWED:
+      case STOP:
+        /* Stop rollers */
+        stopIntake();
         break;
-
-      case STOWING:
-        stowHopper();
-        if (isDeployed(0.0, 0.0)) {
-          currHopperState = HopperState.STOWED;
-        }
-        break;
-
-      case DEPLOYING:
-        expandHopper();
-        if (isDeployed(EXPANDING_SETPOINT, EXPANDING_TOLERANCE)) {
-          if (desiredHopperState == HopperState.INDEXTOSHOOTER) {
-            currHopperState = HopperState.INDEXTOSHOOTER;
-          } else if (desiredHopperState == HopperState.INDEXTOINTAKE) {
-            currHopperState = HopperState.INDEXTOINTAKE;
-          }
-        }
-        break;
-
       case INDEXTOSHOOTER:
+        /* set hopper rollers to index to shooter speed */
         indexToShooter();
         break;
-
       case INDEXTOINTAKE:
+        /* set hopper rollers to index to outtake speed */
         indexToIntake();
         break;
-
       default:
         break;
     }
-    expandingMotorIO.updateInputs(expandingMotorInputs);
+  }
+
+  @Override
+  public void periodic() {
+    
+    /* This method will be called once per scheduler run */
+    handleStateTransition();
+
+    DogLog.log("Hopper/Hopper State", currHopperState.toString());
+    
     rollerMotorIO.updateInputs(rollerMotorIOInputs);
   }
 }
