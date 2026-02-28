d0b0dded73a880dd9d4410f4b3877d7ad0bb3d87
Doug Liu <dougdliu@gmail.com>
2026-02-28 01:11:08 -0700

    1. defaultDrive and shootDrive will return DefaultDriveCmd and ShootDriveCmd 2. Remove unused imports and unnecessary variables

--- src/main/java/frc/robot/subsystems/Superstructure.java
+++ src/main/java/frc/robot/subsystems/Superstructure.java
@@ -1,22 +1,14 @@
 package frc.robot.subsystems;
 
-import static edu.wpi.first.units.Units.MetersPerSecond;
-import static edu.wpi.first.units.Units.RadiansPerSecond;
-import static edu.wpi.first.units.Units.RotationsPerSecond;
-
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
-import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotContainer;
-import frc.robot.generated.TunerConstants;
+import frc.robot.commands.DefaultDriveCmd;
+import frc.robot.commands.ShootDriveCmd;
 import frc.robot.subsystems.climber.ClimberSubsystem;
 import frc.robot.subsystems.hopper.HopperSubsystem;
 import frc.robot.subsystems.intake.IntakeSubsystem;
@@ -24,19 +16,20 @@ import frc.robot.subsystems.intake.IntakeSubsystem.IntakeState;
 import frc.robot.subsystems.shooter.ShooterSubsystem;
 import frc.robot.subsystems.vision.VisionSubsystem;
 import frc.robot.util.Telemetry;
-import frc.robot.util.constants.FieldConstants;
-import frc.robot.util.constants.SwerveConstants;
 import java.util.Optional;
 import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
 
-public class Superstructure {
+public class Superstructure extends SubsystemBase {
 
   public enum SuperState {
     DRIVE,
     INTAKE,
+    OUTTAKE,
     SHOOT
   }
+
+  private SuperState state;
 
   private final CommandSwerveDrivetrain swerve;
   private final IntakeSubsystem intake;
@@ -48,18 +41,12 @@ public class Superstructure {
   private final Telemetry logger;
 
   /* Setting up bindings for necessary control of the swerve drive platform */
-  private final SwerveRequest.FieldCentric drive;
   private final SwerveRequest.SwerveDriveBrake brake;
   private final SwerveRequest.PointWheelsAt point;
-  private final SwerveRequest.RobotCentric driveRobotCentric;
-  private final SwerveRequest.FieldCentricFacingAngle driveMaintainHeading;
 
   /* Used for path following */
   private final SwerveRequest.ApplyFieldSpeeds applyFieldSpeeds;
   private final SwerveRequest.ApplyRobotSpeeds applyRobotSpeeds;
-
-  private final double maxSpeed;
-  private final double maxAngularRate;
 
   private Optional<Rotation2d> currentHeading; // Keeps track of current heading
   private double rotationLastTriggered; // Keeps track of the last time the rotation was triggered
@@ -80,45 +67,14 @@ public class Superstructure {
     this.vision = vision;
     this.container = container;
 
-    /* Instatiate swerve max speed and angular rate */
-    maxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // Max speed at 12 volts
-    maxAngularRate =
-        RotationsPerSecond.of(1).in(RadiansPerSecond); // 1 rotation per second max angular velocity
-
-    /* Instantiate default field centric drive (no need to maintain heading) */
-    drive =
-        new SwerveRequest.FieldCentric()
-            .withDeadband(maxSpeed * 0.05)
-            .withRotationalDeadband(maxAngularRate * 0.05) // Add a 5% deadband
-            .withDriveRequestType(
-                DriveRequestType.OpenLoopVoltage) // Use open-loop control for drive motors
-            .withDesaturateWheelSpeeds(true); // Desaturate wheel speeds to prevent clipping
+    /* Initialize superstate */
+    state = SuperState.DRIVE;
 
     /* Instantiate brake (X-lock swerve wheels) */
     brake = new SwerveRequest.SwerveDriveBrake();
 
     /* Instantiate point (point swerve wheels in a specific direction) */
     point = new SwerveRequest.PointWheelsAt();
-
-    /* Instantiate robot centric drive (forward is based on forward pose of the
-    robot) */
-    driveRobotCentric =
-        new SwerveRequest.RobotCentric().withDriveRequestType(DriveRequestType.OpenLoopVoltage);
-
-    /* Instantiate field centric drive (maintain heading) */
-    driveMaintainHeading =
-        new SwerveRequest.FieldCentricFacingAngle()
-            .withDeadband(maxSpeed * 0.05)
-            .withRotationalDeadband(maxAngularRate * 0.05)
-            .withDriveRequestType(
-                DriveRequestType.OpenLoopVoltage) // Use open-loop control for drive motors
-            .withDesaturateWheelSpeeds(true);
-
-    /* Set the PID constants for the Maintain Heading controller */
-    driveMaintainHeading.HeadingController.setPID(
-        SwerveConstants.HEADING_KP, SwerveConstants.HEADING_KI, SwerveConstants.HEADING_KD);
-    driveMaintainHeading.HeadingController.enableContinuousInput(-Math.PI, Math.PI);
-    driveMaintainHeading.HeadingController.setTolerance(SwerveConstants.HEADING_TOLERANCE);
 
     /* Instantiate the Field and Robot Speeds Swerve Requests */
     applyFieldSpeeds =
@@ -137,134 +93,44 @@ public class Superstructure {
     rotationLastTriggered = 0.0;
 
     /* Instantiate the logger for telemetry */
-    logger = new Telemetry(maxSpeed);
+    logger = new Telemetry();
 
     /* Register the telemetry for the swerve drive */
     swerve.registerTelemetry(logger::telemeterize);
   }
 
-  public Command defaultDriveCmd(
+  public Optional<Rotation2d> getCurrentHeading() { return currentHeading; }
+  public void setCurrentHeading(Optional<Rotation2d> heading) { this.currentHeading = heading; }
+  public double getRotationLastTriggered() { return rotationLastTriggered; }
+  public void setRotationLastTriggered(double t) { this.rotationLastTriggered = t; }
+
+  public Command defaultDrive(
       DoubleSupplier translationSup,
       DoubleSupplier strafeSup,
       DoubleSupplier rotationSup,
       BooleanSupplier halfSpeedSup) {
-    return new RunCommand(
-        () -> {
-          double rawRotation = rotationSup.getAsDouble();
-          var speeds =
-              processJoystickInputs(
-                  translationSup.getAsDouble(),
-                  strafeSup.getAsDouble(),
-                  rawRotation,
-                  halfSpeedSup.getAsBoolean());
-
-          boolean rotationTriggered = Math.abs(rawRotation) > SwerveConstants.SWERVE_DEADBAND;
-          boolean rotationActive =
-              MathUtil.isNear(rotationLastTriggered, Timer.getFPGATimestamp(), 0.1)
-                  && (Math.abs(swerve.getState().Speeds.omegaRadiansPerSecond)
-                      > Math.toRadians(10));
-
-          if (rotationTriggered) {
-            rotationLastTriggered = Timer.getFPGATimestamp();
-          }
-
-          if (rotationTriggered || rotationActive) {
-            setSwerveToRotate(speeds.translation, speeds.strafe, speeds.rotation);
-          } else {
-            setSwerveToMaintainHeading(speeds.translation, speeds.strafe);
-          }
-        },
-        swerve);
+    return new DefaultDriveCmd(
+        swerve,
+        translationSup,
+        strafeSup,
+        rotationSup,
+        halfSpeedSup,
+        this::getCurrentHeading,
+        this::setCurrentHeading,
+        this::getRotationLastTriggered,
+        this::setRotationLastTriggered);
   }
 
-  private static class Speeds {
-    double translation;
-    double strafe;
-    double rotation;
-  }
-
-  private Speeds processJoystickInputs(
-      double rawTranslation, double rawStrafe, double rawRotation, boolean halfSpeed) {
-    double translation = MathUtil.applyDeadband(rawTranslation, SwerveConstants.SWERVE_DEADBAND, 1);
-    double strafe = MathUtil.applyDeadband(rawStrafe, SwerveConstants.SWERVE_DEADBAND, 1);
-    double rotation = MathUtil.applyDeadband(rawRotation, SwerveConstants.SWERVE_DEADBAND, 1);
-
-    translation = Math.copySign(translation * translation, translation);
-    strafe = Math.copySign(strafe * strafe, strafe);
-    rotation = Math.copySign(rotation * rotation, rotation);
-
-    if (halfSpeed) {
-      translation *= 0.35;
-      strafe *= 0.35;
-      rotation *= 0.35;
-    }
-
-    Speeds speeds = new Speeds();
-    speeds.translation = translation * maxSpeed;
-    speeds.strafe = strafe * maxSpeed;
-    speeds.rotation = rotation * maxAngularRate;
-    return speeds;
-  }
-
-  private void setSwerveToRotate(double translation, double strafe, double rotation) {
-    swerve.setControl(
-        drive.withVelocityX(translation).withVelocityY(strafe).withRotationalRate(rotation));
-    currentHeading = Optional.empty();
-  }
-
-  private void setSwerveToMaintainHeading(double translation, double strafe) {
-    if (currentHeading.isEmpty()) {
-      currentHeading = Optional.of(swerve.getState().Pose.getRotation());
-    }
-
-    Optional<Alliance> alliance = DriverStation.getAlliance();
-    if (alliance.isPresent()) {
-      Rotation2d targetDirection =
-          alliance.get() == Alliance.Blue
-              ? currentHeading.get()
-              : currentHeading.get().rotateBy(Rotation2d.fromDegrees(180));
-      swerve.setControl(
-          driveMaintainHeading
-              .withVelocityX(translation)
-              .withVelocityY(strafe)
-              .withTargetDirection(targetDirection));
-    }
-  }
-
-  public Command driveAimedTowardsHubCmd(DoubleSupplier translationSup, DoubleSupplier strafeSup) {
-    return new RunCommand(
-        () -> {
-          double translation = (translationSup.getAsDouble() * maxSpeed) / 3;
-          double strafe = (strafeSup.getAsDouble() * maxSpeed) / 3;
-
-          // Grab the robot's current alliance
-          Optional<Alliance> alliance = DriverStation.getAlliance();
-
-          // Choose which hub to aim to depending on alliance color
-          Translation2d hubToAimTowards =
-              alliance.isPresent() && (alliance.get() == Alliance.Red)
-                  ? FieldConstants.Hub.RED_HUB_CENTER_POSE
-                  : FieldConstants.Hub.BLUE_HUB_CENTER_POSE;
-
-          // Current robot translation (x,y) in field coordinates
-          var robotTranslation = swerve.getState().Pose.getTranslation();
-
-          // Vector from robot to target hub
-          var delta = hubToAimTowards.minus(robotTranslation);
-
-          // Absolute angle in field frame pointing from robot to hub
-          Rotation2d angleToPointAt = new Rotation2d(Math.atan2(delta.getY(), delta.getX()));
-
-          currentHeading = Optional.of(angleToPointAt);
-
-          swerve.setControl(
-              driveMaintainHeading
-                  .withVelocityX(translation)
-                  .withVelocityY(strafe)
-                  .withTargetDirection(angleToPointAt)
-                  .withTargetRateFeedforward(0.1));
-        },
-        swerve);
+  public Command shootDrive(
+      DoubleSupplier translationSup,
+      DoubleSupplier strafeSup
+  ) {
+    return new ShootDriveCmd(
+        swerve,
+        translationSup,
+        strafeSup,
+        this::setCurrentHeading
+    );
   }
 
   public Command swerveBrakeCmd() {
@@ -282,4 +148,45 @@ public class Superstructure {
   public Command stowIntakeCommand() {
     return new InstantCommand(() -> intake.setDesiredState(IntakeState.HOME), intake);
   }
+
+  /* State handling */
+  public void setDesiredSuperState(SuperState state) {
+    this.state = state;
+  }
+
+  public void handleStateTransition() {
+    // State machine logic
+    switch (state) {
+      case DRIVE:
+        // Set Drive to maintain heading
+        // Intake remains in deployed or home based on current intake state
+        // Set Hopper to STOP
+        // Set Shooter to PREPFUEL
+        break;
+      case INTAKE:
+        // Set Drive to maintain heading at reduced speeds
+        // Set Intake to INTAKE
+        // Set Hopper to INDEXTOSHOOTER
+        // Set Shooter to PREPFUEL
+        break;
+      case OUTTAKE:
+        // Set Drive to maintain heading at reduced speeds
+        // Set Intake to OUTTAKE
+        // Set Hopper to INDEXTOINTAKE
+        // Set Shooter to PREPFUEL
+        break;
+      case SHOOT:
+        // Set Drive to point towards target
+        // Set Intake to INTAKE
+        // Set Hopper to INDEXTOSHOOTER
+        // Set Shooter to SHOOT
+        break;
+    }
+  }
+
+  @Override
+  public void periodic() {
+
+  }
+
 }
