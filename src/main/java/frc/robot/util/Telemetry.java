af3b38e12a5d4f078ce937bf95307d2737856332
Doug Liu <dougdliu@gmail.com>
2026-02-28 01:09:07 -0700

    Commented out Intake and Hopper extension sim calibration. Don't want these to execute in the actual robot code.

--- src/main/java/frc/robot/util/Telemetry.java
+++ src/main/java/frc/robot/util/Telemetry.java
@@ -8,15 +8,13 @@ import edu.wpi.first.math.util.Units;
 
 public class Telemetry {
   /**
-   * Construct a telemetry object, with the specified max speed of the robot
-   *
-   * @param maxSpeed Maximum speed in meters per second
+   * Construct a telemetry object.
    */
-  public Telemetry(double maxSpeed) {
+  public Telemetry() {
     // SignalLogger.start();
   }
 
-  /** Accept the swerve drive state and telemeterize it to SmartDashboard and SignalLogger. */
+  /** Accept the swerve drive state and telemeterize it to DogLog. */
   public void telemeterize(SwerveDriveState state) {
     /* Telemeterize the swerve drive state */
     DogLog.log("Pose", state.Pose);
@@ -27,22 +25,22 @@ public class Telemetry {
     DogLog.log("Timestamp", state.Timestamp);
     DogLog.log("OdometryFrequency", 1.0 / state.OdometryPeriod);
 
-    DogLog.log(
-        "FinalIntakePose",
-        new Pose3d[] {
-          new Pose3d(
-              0.2949,
-              0,
-              0.20099,
-              // new Rotation3d(0.0, Math.sin(Timer.getTimestamp()) - 1, 0.0),
-              new Rotation3d(0.0, Units.degreesToRadians(-125), 0.0))
-        });
-    DogLog.log(
-        "FinalExtendingHopperPose",
-        new Pose3d[] {
-          new Pose3d(
-              // 0.05 + (((Math.sin(Timer.getTimestamp()) + 1) / 2) * Units.inchesToMeters(12.562)),
-              0.05, 0.06375, 0.3, new Rotation3d(0.0, 0.0, 0.0))
-        });
+    // DogLog.log(
+    //     "FinalIntakePose",
+    //     new Pose3d[] {
+    //       new Pose3d(
+    //           0.2949,
+    //           0,
+    //           0.20099,
+    //           // new Rotation3d(0.0, Math.sin(Timer.getTimestamp()) - 1, 0.0),
+    //           new Rotation3d(0.0, Units.degreesToRadians(-125), 0.0))
+    //     });
+    // DogLog.log(
+    //     "FinalExtendingHopperPose",
+    //     new Pose3d[] {
+    //       new Pose3d(
+    //           // 0.05 + (((Math.sin(Timer.getTimestamp()) + 1) / 2) * Units.inchesToMeters(12.562)),
+    //           0.05, 0.06375, 0.3, new Rotation3d(0.0, 0.0, 0.0))
+    //     });
   }
 }
