2ac92de6e24d9db5d1d24fd32cdbe14e12d69da4
Doug Liu <dougdliu@gmail.com>
2026-03-13 03:53:52 -0700

    Inverted the hopper, kicker, and shooter motors

--- src/main/java/frc/robot/util/constants/HopperConstants.java
+++ src/main/java/frc/robot/util/constants/HopperConstants.java
@@ -8,6 +8,7 @@ import com.ctre.phoenix6.configs.OpenLoopRampsConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.configs.VoltageConfigs;
+import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
 public final class HopperConstants {
@@ -20,22 +21,24 @@ public final class HopperConstants {
   public static final double HOPPER_ROLLER_RPM = 1000.0;
   public static final double HOPPER_ROLLER_REVERSE_RPM = -1000.0;
 
-  public static final TalonFXConfiguration HOPPER_ROLLER_TALONFX_CONFIG =
-      new TalonFXConfiguration()
-          .withCurrentLimits(
-              new CurrentLimitsConfigs()
-                  .withStatorCurrentLimitEnable(true)
-                  .withStatorCurrentLimit(HOPPER_ROLLER_STATOR_CURRENT_LIMIT)
-                  .withSupplyCurrentLimitEnable(true)
-                  .withSupplyCurrentLimit(HOPPER_ROLLER_SUPPLY_CURRENT_LIMIT))
-          .withVoltage(
-              new VoltageConfigs()
-                  .withPeakForwardVoltage(HOPPER_ROLLER_MAX_VOLTAGE)
-                  .withPeakReverseVoltage(-HOPPER_ROLLER_MAX_VOLTAGE))
-          .withOpenLoopRamps(
-              new OpenLoopRampsConfigs()
-                  .withDutyCycleOpenLoopRampPeriod(HOPPER_ROLLER_RAMP_RATE)
-                  .withTorqueOpenLoopRampPeriod(HOPPER_ROLLER_RAMP_RATE)
-                  .withVoltageOpenLoopRampPeriod(HOPPER_ROLLER_RAMP_RATE))
-          .withMotorOutput(new MotorOutputConfigs().withNeutralMode(NeutralModeValue.Coast));
+  public static final TalonFXConfiguration HOPPER_ROLLER_TALONFX_CONFIG = new TalonFXConfiguration()
+      .withCurrentLimits(
+          new CurrentLimitsConfigs()
+              .withStatorCurrentLimitEnable(true)
+              .withStatorCurrentLimit(HOPPER_ROLLER_STATOR_CURRENT_LIMIT)
+              .withSupplyCurrentLimitEnable(true)
+              .withSupplyCurrentLimit(HOPPER_ROLLER_SUPPLY_CURRENT_LIMIT))
+      .withVoltage(
+          new VoltageConfigs()
+              .withPeakForwardVoltage(HOPPER_ROLLER_MAX_VOLTAGE)
+              .withPeakReverseVoltage(-HOPPER_ROLLER_MAX_VOLTAGE))
+      .withOpenLoopRamps(
+          new OpenLoopRampsConfigs()
+              .withDutyCycleOpenLoopRampPeriod(HOPPER_ROLLER_RAMP_RATE)
+              .withTorqueOpenLoopRampPeriod(HOPPER_ROLLER_RAMP_RATE)
+              .withVoltageOpenLoopRampPeriod(HOPPER_ROLLER_RAMP_RATE))
+      .withMotorOutput(
+          new MotorOutputConfigs()
+              .withNeutralMode(NeutralModeValue.Coast)
+              .withInverted(InvertedValue.Clockwise_Positive));
 }
--- src/main/java/frc/robot/util/constants/ShooterConstants.java
+++ src/main/java/frc/robot/util/constants/ShooterConstants.java
@@ -12,6 +12,7 @@ import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.configs.VoltageConfigs;
 import com.ctre.phoenix6.signals.GravityTypeValue;
+import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.ctre.phoenix6.signals.SensorDirectionValue;
 import com.ctre.phoenix6.signals.StaticFeedforwardSignValue;
@@ -57,7 +58,8 @@ public final class ShooterConstants {
     }
   }
 
-  public record ShooterSetpoint(double shooterRPM, double hoodAngleDeg) {}
+  public record ShooterSetpoint(double shooterRPM, double hoodAngleDeg) {
+  }
 
   public record ShooterZone(
       double minDistanceMeters, double maxDistanceMeters, ShooterSetpoint setpoint) {
@@ -67,12 +69,11 @@ public final class ShooterConstants {
   }
 
   // Example 3 zones (TODO: Tune these values in real world testing)
-  public static final List<ShooterZone> SHOOTER_ZONES =
-      List.of(
-          new ShooterZone(0.0, 2.0, new ShooterSetpoint(3000.0, 0.12)), // close
-          new ShooterZone(2.0, 4.0, new ShooterSetpoint(3800.0, 0.28)), // mid
-          new ShooterZone(4.0, Double.POSITIVE_INFINITY, new ShooterSetpoint(4500.0, 0.42)) // far
-          );
+  public static final List<ShooterZone> SHOOTER_ZONES = List.of(
+      new ShooterZone(0.0, 2.0, new ShooterSetpoint(3000.0, 0.12)), // close
+      new ShooterZone(2.0, 4.0, new ShooterSetpoint(3800.0, 0.28)), // mid
+      new ShooterZone(4.0, Double.POSITIVE_INFINITY, new ShooterSetpoint(4500.0, 0.42)) // far
+  );
 
   public static ShooterSetpoint getSetpointForDistance(double distanceMeters) {
     for (ShooterZone zone : SHOOTER_ZONES) {
@@ -83,190 +84,198 @@ public final class ShooterConstants {
     return SHOOTER_ZONES.get(SHOOTER_ZONES.size() - 1).setpoint();
   }
 
-  public static final TalonFXConfiguration SHOOTER_LEAD_TALONFX_CONFIG =
-      new TalonFXConfiguration()
-          .withCurrentLimits(
-              new CurrentLimitsConfigs()
-                  .withStatorCurrentLimitEnable(true)
-                  .withStatorCurrentLimit(Amps.of(80))
-                  .withSupplyCurrentLimitEnable(true)
-                  .withSupplyCurrentLimit(Amps.of(40))
-                  .withSupplyCurrentLowerLimit(20)
-                  .withSupplyCurrentLowerTime(1))
-          .withVoltage(new VoltageConfigs().withPeakForwardVoltage(10).withPeakReverseVoltage(-10))
-          .withOpenLoopRamps(
-              new OpenLoopRampsConfigs()
-                  .withDutyCycleOpenLoopRampPeriod(Seconds.of(0.1))
-                  .withTorqueOpenLoopRampPeriod(Seconds.of(0.1))
-                  .withVoltageOpenLoopRampPeriod(Seconds.of(0.1)))
-          .withMotorOutput(new MotorOutputConfigs().withNeutralMode(NeutralModeValue.Coast))
-          .withSlot0(
-              new Slot0Configs()
-                  .withKS(4.325)
-                  .withKV(0.013)
-                  .withKA(0.0)
-                  .withKP(1.5)
-                  .withKI(0.0)
-                  .withKD(0.0));
+  public static final TalonFXConfiguration SHOOTER_LEAD_TALONFX_CONFIG = new TalonFXConfiguration()
+      .withCurrentLimits(
+          new CurrentLimitsConfigs()
+              .withStatorCurrentLimitEnable(true)
+              .withStatorCurrentLimit(Amps.of(80))
+              .withSupplyCurrentLimitEnable(true)
+              .withSupplyCurrentLimit(Amps.of(40))
+              .withSupplyCurrentLowerLimit(20)
+              .withSupplyCurrentLowerTime(1))
+      .withVoltage(
+          new VoltageConfigs()
+              .withPeakForwardVoltage(12)
+              .withPeakReverseVoltage(-12))
+      .withOpenLoopRamps(
+          new OpenLoopRampsConfigs()
+              .withDutyCycleOpenLoopRampPeriod(Seconds.of(0.1))
+              .withTorqueOpenLoopRampPeriod(Seconds.of(0.1))
+              .withVoltageOpenLoopRampPeriod(Seconds.of(0.1)))
+      .withMotorOutput(
+          new MotorOutputConfigs()
+              .withNeutralMode(NeutralModeValue.Coast)
+              .withInverted(InvertedValue.Clockwise_Positive))
+      .withSlot0(
+          new Slot0Configs()
+              .withKS(4.325)
+              .withKV(0.013)
+              .withKA(0.0)
+              .withKP(1.5)
+              .withKI(0.0)
+              .withKD(0.0));
 
-  public static final SparkBaseConfig SHOOTER_LEAD_SPARKMAX_CONFIG =
-      new SparkMaxConfig()
-          .apply(
-              new SparkMaxConfig()
-                  .voltageCompensation(10)
-                  .smartCurrentLimit(60, 20)
-                  .secondaryCurrentLimit(80)
-                  .openLoopRampRate(0.1)
-                  .idleMode(IdleMode.kCoast))
-          .apply(
-              new ClosedLoopConfig()
-                  .feedbackSensor(FeedbackSensor.kAlternateOrExternalEncoder)
-                  .outputRange(-1, 1)
-                  .pid(0.0, 0.0, 0.0, ClosedLoopSlot.kSlot0)
-                  .apply(
-                      new MAXMotionConfig()
-                          .cruiseVelocity(4000, ClosedLoopSlot.kSlot0)
-                          .maxAcceleration(8000, ClosedLoopSlot.kSlot0)
-                          .allowedProfileError(40, ClosedLoopSlot.kSlot0)));
+  public static final SparkBaseConfig SHOOTER_LEAD_SPARKMAX_CONFIG = new SparkMaxConfig()
+      .apply(
+          new SparkMaxConfig()
+              .voltageCompensation(10)
+              .smartCurrentLimit(60, 20)
+              .secondaryCurrentLimit(80)
+              .openLoopRampRate(0.1)
+              .idleMode(IdleMode.kCoast))
+      .apply(
+          new ClosedLoopConfig()
+              .feedbackSensor(FeedbackSensor.kAlternateOrExternalEncoder)
+              .outputRange(-1, 1)
+              .pid(0.0, 0.0, 0.0, ClosedLoopSlot.kSlot0)
+              .apply(
+                  new MAXMotionConfig()
+                      .cruiseVelocity(4000, ClosedLoopSlot.kSlot0)
+                      .maxAcceleration(8000, ClosedLoopSlot.kSlot0)
+                      .allowedProfileError(40, ClosedLoopSlot.kSlot0)));
 
-  public static final TalonFXConfiguration SHOOTER_FOLLOW_TALONFX_CONFIG =
-      new TalonFXConfiguration()
-          .withCurrentLimits(
-              new CurrentLimitsConfigs()
-                  .withStatorCurrentLimitEnable(true)
-                  .withStatorCurrentLimit(Amps.of(80))
-                  .withSupplyCurrentLimitEnable(true)
-                  .withSupplyCurrentLimit(Amps.of(40))
-                  .withSupplyCurrentLowerLimit(20)
-                  .withSupplyCurrentLowerTime(1))
-          .withVoltage(new VoltageConfigs().withPeakForwardVoltage(10).withPeakReverseVoltage(-10))
-          .withOpenLoopRamps(
-              new OpenLoopRampsConfigs()
-                  .withDutyCycleOpenLoopRampPeriod(Seconds.of(0.1))
-                  .withTorqueOpenLoopRampPeriod(Seconds.of(0.1))
-                  .withVoltageOpenLoopRampPeriod(Seconds.of(0.1)))
-          .withMotorOutput(new MotorOutputConfigs().withNeutralMode(NeutralModeValue.Coast));
+  public static final TalonFXConfiguration SHOOTER_FOLLOW_TALONFX_CONFIG = new TalonFXConfiguration()
+      .withCurrentLimits(
+          new CurrentLimitsConfigs()
+              .withStatorCurrentLimitEnable(true)
+              .withStatorCurrentLimit(Amps.of(80))
+              .withSupplyCurrentLimitEnable(true)
+              .withSupplyCurrentLimit(Amps.of(40))
+              .withSupplyCurrentLowerLimit(20)
+              .withSupplyCurrentLowerTime(1))
+      .withVoltage(
+          new VoltageConfigs()
+              .withPeakForwardVoltage(12)
+              .withPeakReverseVoltage(-12))
+      .withOpenLoopRamps(
+          new OpenLoopRampsConfigs()
+              .withDutyCycleOpenLoopRampPeriod(Seconds.of(0.1))
+              .withTorqueOpenLoopRampPeriod(Seconds.of(0.1))
+              .withVoltageOpenLoopRampPeriod(Seconds.of(0.1)))
+      .withMotorOutput(
+          new MotorOutputConfigs()
+              .withNeutralMode(NeutralModeValue.Coast));
 
-  public static final SparkBaseConfig SHOOTER_FOLLOW_SPARKMAX_CONFIG =
-      new SparkMaxConfig()
-          .apply(
-              new SparkMaxConfig()
-                  .voltageCompensation(10)
-                  .smartCurrentLimit(60, 20)
-                  .secondaryCurrentLimit(80)
-                  .openLoopRampRate(0.1)
-                  .idleMode(IdleMode.kCoast)
-                  .follow(SHOOTER_LEAD_MOTOR_ID));
+  public static final SparkBaseConfig SHOOTER_FOLLOW_SPARKMAX_CONFIG = new SparkMaxConfig()
+      .apply(
+          new SparkMaxConfig()
+              .voltageCompensation(10)
+              .smartCurrentLimit(60, 20)
+              .secondaryCurrentLimit(80)
+              .openLoopRampRate(0.1)
+              .idleMode(IdleMode.kCoast)
+              .follow(SHOOTER_LEAD_MOTOR_ID));
 
-  public static final TalonFXConfiguration SHOOTER_KICKER_TALONFX_CONFIG =
-      new TalonFXConfiguration()
-          .withCurrentLimits(
-              new CurrentLimitsConfigs()
-                  .withStatorCurrentLimitEnable(true)
-                  .withStatorCurrentLimit(Amps.of(80))
-                  .withSupplyCurrentLimitEnable(true)
-                  .withSupplyCurrentLimit(Amps.of(60))
-                  .withSupplyCurrentLowerLimit(Amps.of(40))
-                  .withSupplyCurrentLowerTime(1))
-          .withVoltage(new VoltageConfigs().withPeakForwardVoltage(10).withPeakReverseVoltage(-10))
-          .withOpenLoopRamps(
-              new OpenLoopRampsConfigs()
-                  .withDutyCycleOpenLoopRampPeriod(Seconds.of(0.1))
-                  .withTorqueOpenLoopRampPeriod(Seconds.of(0.1))
-                  .withVoltageOpenLoopRampPeriod(Seconds.of(0.1)))
-          .withMotorOutput(new MotorOutputConfigs().withNeutralMode(NeutralModeValue.Coast))
-          .withSlot0(
-              new Slot0Configs()
-                  .withKS(14.4)
-                  .withKV(0.11)
-                  .withKA(0.0)
-                  .withKP(1.5)
-                  .withKI(0.0)
-                  .withKD(0.0));
+  public static final TalonFXConfiguration SHOOTER_KICKER_TALONFX_CONFIG = new TalonFXConfiguration()
+      .withCurrentLimits(
+          new CurrentLimitsConfigs()
+              .withStatorCurrentLimitEnable(true)
+              .withStatorCurrentLimit(Amps.of(80))
+              .withSupplyCurrentLimitEnable(true)
+              .withSupplyCurrentLimit(Amps.of(60))
+              .withSupplyCurrentLowerLimit(Amps.of(40))
+              .withSupplyCurrentLowerTime(1))
+      .withVoltage(
+          new VoltageConfigs()
+              .withPeakForwardVoltage(12)
+              .withPeakReverseVoltage(-12))
+      .withOpenLoopRamps(
+          new OpenLoopRampsConfigs()
+              .withDutyCycleOpenLoopRampPeriod(Seconds.of(0.1))
+              .withTorqueOpenLoopRampPeriod(Seconds.of(0.1))
+              .withVoltageOpenLoopRampPeriod(Seconds.of(0.1)))
+      .withMotorOutput(
+          new MotorOutputConfigs()
+              .withNeutralMode(NeutralModeValue.Coast)
+              .withInverted(InvertedValue.Clockwise_Positive))
+      .withSlot0(
+          new Slot0Configs()
+              .withKS(14.4)
+              .withKV(0.11)
+              .withKA(0.0)
+              .withKP(1.5)
+              .withKI(0.0)
+              .withKD(0.0));
 
-  public static final SparkBaseConfig SHOOTER_KICKER_SPARKMAX_CONFIG =
-      new SparkMaxConfig()
-          .apply(
-              new SparkMaxConfig()
-                  .voltageCompensation(10)
-                  .smartCurrentLimit(20, 10)
-                  .secondaryCurrentLimit(40)
-                  .openLoopRampRate(0.1)
-                  .idleMode(IdleMode.kCoast))
-          .apply(
-              new ClosedLoopConfig()
-                  .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
-                  .outputRange(-1, 1)
-                  .pid(0.0, 0.0, 0.0, ClosedLoopSlot.kSlot0)
-                  .apply(
-                      new MAXMotionConfig()
-                          .cruiseVelocity(4000, ClosedLoopSlot.kSlot0)
-                          .maxAcceleration(8000, ClosedLoopSlot.kSlot0)
-                          .allowedProfileError(40, ClosedLoopSlot.kSlot0)));
+  public static final SparkBaseConfig SHOOTER_KICKER_SPARKMAX_CONFIG = new SparkMaxConfig()
+      .apply(
+          new SparkMaxConfig()
+              .voltageCompensation(10)
+              .smartCurrentLimit(20, 10)
+              .secondaryCurrentLimit(40)
+              .openLoopRampRate(0.1)
+              .idleMode(IdleMode.kCoast))
+      .apply(
+          new ClosedLoopConfig()
+              .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+              .outputRange(-1, 1)
+              .pid(0.0, 0.0, 0.0, ClosedLoopSlot.kSlot0)
+              .apply(
+                  new MAXMotionConfig()
+                      .cruiseVelocity(4000, ClosedLoopSlot.kSlot0)
+                      .maxAcceleration(8000, ClosedLoopSlot.kSlot0)
+                      .allowedProfileError(40, ClosedLoopSlot.kSlot0)));
 
-  public static final TalonFXConfiguration SHOOTER_HOOD_TALONFX_CONFIG =
-      new TalonFXConfiguration()
-          .withCurrentLimits(
-              new CurrentLimitsConfigs()
-                  .withStatorCurrentLimitEnable(true)
-                  .withStatorCurrentLimit(Amps.of(20))
-                  .withSupplyCurrentLimitEnable(true)
-                  .withSupplyCurrentLimit(Amps.of(10)))
-          .withVoltage(new VoltageConfigs().withPeakForwardVoltage(10).withPeakReverseVoltage(-10))
-          .withOpenLoopRamps(
-              new OpenLoopRampsConfigs()
-                  .withDutyCycleOpenLoopRampPeriod(Seconds.of(0.1))
-                  .withTorqueOpenLoopRampPeriod(Seconds.of(0.1))
-                  .withVoltageOpenLoopRampPeriod(Seconds.of(0.1)))
-          .withMotorOutput(new MotorOutputConfigs().withNeutralMode(NeutralModeValue.Brake))
-          .withSlot0(
-              new Slot0Configs()
-                  .withKS(0.0)
-                  .withKV(0.0)
-                  .withKA(0.0)
-                  .withKG(0.4)
-                  .withGravityType(GravityTypeValue.Elevator_Static)
-                  .withKP(8.0)
-                  .withKI(0.0)
-                  .withKD(0.1)
-                  .withStaticFeedforwardSign(StaticFeedforwardSignValue.UseClosedLoopSign));
+  public static final TalonFXConfiguration SHOOTER_HOOD_TALONFX_CONFIG = new TalonFXConfiguration()
+      .withCurrentLimits(
+          new CurrentLimitsConfigs()
+              .withStatorCurrentLimitEnable(true)
+              .withStatorCurrentLimit(Amps.of(20))
+              .withSupplyCurrentLimitEnable(true)
+              .withSupplyCurrentLimit(Amps.of(10)))
+      .withVoltage(new VoltageConfigs().withPeakForwardVoltage(10).withPeakReverseVoltage(-10))
+      .withOpenLoopRamps(
+          new OpenLoopRampsConfigs()
+              .withDutyCycleOpenLoopRampPeriod(Seconds.of(0.1))
+              .withTorqueOpenLoopRampPeriod(Seconds.of(0.1))
+              .withVoltageOpenLoopRampPeriod(Seconds.of(0.1)))
+      .withMotorOutput(new MotorOutputConfigs().withNeutralMode(NeutralModeValue.Brake))
+      .withSlot0(
+          new Slot0Configs()
+              .withKS(0.0)
+              .withKV(0.0)
+              .withKA(0.0)
+              .withKG(0.4)
+              .withGravityType(GravityTypeValue.Elevator_Static)
+              .withKP(8.0)
+              .withKI(0.0)
+              .withKD(0.1)
+              .withStaticFeedforwardSign(StaticFeedforwardSignValue.UseClosedLoopSign));
 
-  public static final CANcoderConfiguration SHOOTER_HOOD_CANCODER_CONFIG =
-      new CANcoderConfiguration()
-          .withMagnetSensor(
-              new MagnetSensorConfigs()
-                  // choose one; common is signed ±0.5 rotations
-                  .withAbsoluteSensorDiscontinuityPoint(0.5)
-                  .withSensorDirection(SensorDirectionValue.CounterClockwise_Positive)
-                  // set this to your calibrated zero (rotations)
-                  .withMagnetOffset(0.0));
+  public static final CANcoderConfiguration SHOOTER_HOOD_CANCODER_CONFIG = new CANcoderConfiguration()
+      .withMagnetSensor(
+          new MagnetSensorConfigs()
+              // choose one; common is signed ±0.5 rotations
+              .withAbsoluteSensorDiscontinuityPoint(0.5)
+              .withSensorDirection(SensorDirectionValue.CounterClockwise_Positive)
+              // set this to your calibrated zero (rotations)
+              .withMagnetOffset(0.0));
 
-  public static final SparkBaseConfig SHOOTER_HOOD_SPARKMAX_CONFIG =
-      new SparkMaxConfig()
-          .apply(
-              new SparkMaxConfig()
-                  .voltageCompensation(10)
-                  .smartCurrentLimit(15, 10)
-                  .secondaryCurrentLimit(30)
-                  .openLoopRampRate(0.1)
-                  .idleMode(IdleMode.kBrake))
-          .apply(new AbsoluteEncoderConfig().zeroOffset(0).inverted(false))
-          .apply(
-              new ClosedLoopConfig()
-                  .feedbackSensor(FeedbackSensor.kAbsoluteEncoder)
-                  .pid(0.0, 0.0, 0.0, ClosedLoopSlot.kSlot0)
-                  .outputRange(-1, 1)
-                  .apply(
-                      new FeedForwardConfig()
-                          .kS(0.0, ClosedLoopSlot.kSlot0)
-                          .kV(0.0, ClosedLoopSlot.kSlot0)
-                          .kA(0.0, ClosedLoopSlot.kSlot0)
-                          .kG(0.0, ClosedLoopSlot.kSlot0))
-                  .apply(
-                      new MAXMotionConfig()
-                          .cruiseVelocity(4000, ClosedLoopSlot.kSlot0)
-                          .maxAcceleration(8000, ClosedLoopSlot.kSlot0)
-                          .allowedProfileError(5, ClosedLoopSlot.kSlot0)
-                          .positionMode(
-                              MAXMotionPositionMode.kMAXMotionTrapezoidal, ClosedLoopSlot.kSlot0)));
+  public static final SparkBaseConfig SHOOTER_HOOD_SPARKMAX_CONFIG = new SparkMaxConfig()
+      .apply(
+          new SparkMaxConfig()
+              .voltageCompensation(10)
+              .smartCurrentLimit(15, 10)
+              .secondaryCurrentLimit(30)
+              .openLoopRampRate(0.1)
+              .idleMode(IdleMode.kBrake))
+      .apply(new AbsoluteEncoderConfig().zeroOffset(0).inverted(false))
+      .apply(
+          new ClosedLoopConfig()
+              .feedbackSensor(FeedbackSensor.kAbsoluteEncoder)
+              .pid(0.0, 0.0, 0.0, ClosedLoopSlot.kSlot0)
+              .outputRange(-1, 1)
+              .apply(
+                  new FeedForwardConfig()
+                      .kS(0.0, ClosedLoopSlot.kSlot0)
+                      .kV(0.0, ClosedLoopSlot.kSlot0)
+                      .kA(0.0, ClosedLoopSlot.kSlot0)
+                      .kG(0.0, ClosedLoopSlot.kSlot0))
+              .apply(
+                  new MAXMotionConfig()
+                      .cruiseVelocity(4000, ClosedLoopSlot.kSlot0)
+                      .maxAcceleration(8000, ClosedLoopSlot.kSlot0)
+                      .allowedProfileError(5, ClosedLoopSlot.kSlot0)
+                      .positionMode(
+                          MAXMotionPositionMode.kMAXMotionTrapezoidal, ClosedLoopSlot.kSlot0)));
 }
