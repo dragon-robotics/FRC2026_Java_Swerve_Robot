38438e49ec856190a38152e7f9bd4102c6908ffb
Doug Liu <dougdliu@gmail.com>
2026-03-29 01:58:48 -0700

    Minimized telemetry to only pose data

--- src/main/java/frc/robot/util/Telemetry.java
+++ src/main/java/frc/robot/util/Telemetry.java
@@ -13,7 +13,7 @@ public class Telemetry {
   public void telemeterize(SwerveDriveState state) {
     /* Telemeterize the swerve drive state */
     DogLog.log("Pose", state.Pose);
-    DogLog.log("Speeds", state.Speeds);
+    // DogLog.log("Speeds", state.Speeds);
     // DogLog.log("ModuleStates", state.ModuleStates);
     // DogLog.log("ModuleTargets", state.ModuleTargets);
     // DogLog.log("ModulePositions", state.ModulePositions);
