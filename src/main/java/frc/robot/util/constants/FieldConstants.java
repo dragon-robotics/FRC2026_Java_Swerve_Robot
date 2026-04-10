1012e297b3f79d79a5887ee1c2c0558392414f0e
Doug Liu <dougdliu@gmail.com>
2026-04-10 00:22:28 -0700

    Removed all the single tags to improve pose estimation consistency

--- src/main/java/frc/robot/util/constants/FieldConstants.java
+++ src/main/java/frc/robot/util/constants/FieldConstants.java
@@ -21,7 +21,7 @@ public final class FieldConstants {
         Filesystem.getDeployDirectory().getPath(),
         "apriltags",
         "welded",
-        "2026-rebuilt-welded.json");
+        "2026-rebuilt-welded-no-single.json");
     AprilTagFieldLayout defaultLayout = null;
     try {
       defaultLayout = new AprilTagFieldLayout(defaultPath);
