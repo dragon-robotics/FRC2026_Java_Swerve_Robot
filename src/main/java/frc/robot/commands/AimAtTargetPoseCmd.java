b33d074a60e29bf75bca378f8d586e76047e9442
Doug Liu <dougdliu@gmail.com>
2026-06-04 02:35:59 -0700

    Spotless linting

--- src/main/java/frc/robot/commands/AimAtTargetPoseCmd.java
+++ src/main/java/frc/robot/commands/AimAtTargetPoseCmd.java
@@ -4,17 +4,14 @@
 
 package frc.robot.commands;
 
-import java.util.Optional;
-import java.util.function.Consumer;
-
-import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
-import com.ctre.phoenix6.swerve.SwerveRequest;
-
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import static edu.wpi.first.units.Units.MetersPerSecond;
 import static edu.wpi.first.units.Units.RadiansPerSecond;
 import static edu.wpi.first.units.Units.RotationsPerSecond;
+
+import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.swerve.SwerveRequest;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -22,6 +19,8 @@ import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.util.constants.FieldConstants;
 import frc.robot.util.constants.SwerveConstants;
+import java.util.Optional;
+import java.util.function.Consumer;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class AimAtTargetPoseCmd extends Command {
@@ -41,13 +40,15 @@ public class AimAtTargetPoseCmd extends Command {
     this.setCurrentHeading = setCurrentHeading;
 
     maxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // Max speed at 12 volts
-    maxAngularRate = RotationsPerSecond.of(1).in(RadiansPerSecond); // 1 rotation per second max angular velocity
+    maxAngularRate =
+        RotationsPerSecond.of(1).in(RadiansPerSecond); // 1 rotation per second max angular velocity
 
-    driveMaintainHeading = new SwerveRequest.FieldCentricFacingAngle()
-        .withDeadband(maxSpeed * 0.05)
-        .withRotationalDeadband(maxAngularRate * 0.05)
-        .withDriveRequestType(DriveRequestType.OpenLoopVoltage)
-        .withDesaturateWheelSpeeds(true);
+    driveMaintainHeading =
+        new SwerveRequest.FieldCentricFacingAngle()
+            .withDeadband(maxSpeed * 0.05)
+            .withRotationalDeadband(maxAngularRate * 0.05)
+            .withDriveRequestType(DriveRequestType.OpenLoopVoltage)
+            .withDesaturateWheelSpeeds(true);
 
     /* Set the PID constants for the Maintain Heading controller */
     driveMaintainHeading.HeadingController.setPID(
@@ -61,8 +62,7 @@ public class AimAtTargetPoseCmd extends Command {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {
-  }
+  public void initialize() {}
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
@@ -71,9 +71,10 @@ public class AimAtTargetPoseCmd extends Command {
     Optional<Alliance> alliance = DriverStation.getAlliance();
 
     // Choose which hub to aim to depending on alliance color
-    Translation2d hubToAimTowards = alliance.isPresent() && (alliance.get() == Alliance.Red)
-        ? FieldConstants.Hub.RED_CENTER_POSE
-        : FieldConstants.Hub.BLUE_CENTER_POSE;
+    Translation2d hubToAimTowards =
+        alliance.isPresent() && (alliance.get() == Alliance.Red)
+            ? FieldConstants.Hub.RED_CENTER_POSE
+            : FieldConstants.Hub.BLUE_CENTER_POSE;
 
     // Current robot translation (x,y) in field coordinates
     var robotTranslation = swerve.getState().Pose.getTranslation();
@@ -93,8 +94,7 @@ public class AimAtTargetPoseCmd extends Command {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {
-  }
+  public void end(boolean interrupted) {}
 
   // Returns true when the command should end.
   @Override
