06f7bdd787bf89899da1e0aad82609d60efce51f
Doug Liu <dougdliu@gmail.com>
2026-06-20 01:39:23 -0700

    Changed Pathplanner PID value back to LYNK's value

--- src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -210,9 +210,9 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
                       .withWheelForceFeedforwardsY(feedforwards.robotRelativeForcesYNewtons())),
           new PPHolonomicDriveController(
               // PID constants for translation
-              new PIDConstants(10, 0, 0.1),
+              new PIDConstants(5.0, 0.0, 0.0),
               // PID constants for rotation
-              new PIDConstants(7, 0, 0.1)),
+              new PIDConstants(4.5, 0.0, 0.0)),
           config,
           // Assume the path needs to be flipped for Red vs Blue, this is normally the
           // case
