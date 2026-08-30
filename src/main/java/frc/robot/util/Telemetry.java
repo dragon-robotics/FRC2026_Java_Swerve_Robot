ceba2125a5c0bc710387a67bb802bebfd50d1bdc
cpc0722 <cesar.parra0722@gmail.com>
2026-08-29 22:16:57 -0700

    Added swerve telemetry for debugging

--- src/main/java/frc/robot/util/Telemetry.java
+++ src/main/java/frc/robot/util/Telemetry.java
@@ -13,30 +13,30 @@ public class Telemetry {
   public void telemeterize(SwerveDriveState state) {
     /* Telemeterize the swerve drive state */
     DogLog.log("Pose", state.Pose);
-    // DogLog.log("Speeds", state.Speeds);
-    // DogLog.log("ModuleStates", state.ModuleStates);
-    // DogLog.log("ModuleTargets", state.ModuleTargets);
-    // DogLog.log("ModulePositions", state.ModulePositions);
-    // DogLog.log("Timestamp", state.Timestamp);
-    // DogLog.log("OdometryFrequency", 1.0 / state.OdometryPeriod);
+    DogLog.log("Speeds", state.Speeds);
+    DogLog.log("ModuleStates", state.ModuleStates);
+    DogLog.log("ModuleTargets", state.ModuleTargets);
+    DogLog.log("ModulePositions", state.ModulePositions);
+    DogLog.log("Timestamp", state.Timestamp);
+    DogLog.log("OdometryFrequency", 1.0 / state.OdometryPeriod);
 
     // DogLog.log(
-    //     "FinalIntakePose",
-    //     new Pose3d[] {
-    //       new Pose3d(
-    //           0.2949,
-    //           0,
-    //           0.20099,
-    //           // new Rotation3d(0.0, Math.sin(Timer.getTimestamp()) - 1, 0.0),
-    //           new Rotation3d(0.0, Units.degreesToRadians(-125), 0.0))
-    //     });
+    // "FinalIntakePose",
+    // new Pose3d[] {
+    // new Pose3d(
+    // 0.2949,
+    // 0,
+    // 0.20099,
+    // // new Rotation3d(0.0, Math.sin(Timer.getTimestamp()) - 1, 0.0),
+    // new Rotation3d(0.0, Units.degreesToRadians(-125), 0.0))
+    // });
     // DogLog.log(
-    //     "FinalExtendingHopperPose",
-    //     new Pose3d[] {
-    //       new Pose3d(
-    //           // 0.05 + (((Math.sin(Timer.getTimestamp()) + 1) / 2) *
+    // "FinalExtendingHopperPose",
+    // new Pose3d[] {
+    // new Pose3d(
+    // // 0.05 + (((Math.sin(Timer.getTimestamp()) + 1) / 2) *
     // Units.inchesToMeters(12.562)),
-    //           0.05, 0.06375, 0.3, new Rotation3d(0.0, 0.0, 0.0))
-    //     });
+    // 0.05, 0.06375, 0.3, new Rotation3d(0.0, 0.0, 0.0))
+    // });
   }
 }
