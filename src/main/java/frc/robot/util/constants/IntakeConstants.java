0c7232e175eb3e3644a439f930b0db81250c4820
Doug Liu <dougdliu@gmail.com>
2026-09-13 01:42:36 -0700

    1. Increased intake roller speed max speed to 90% 2. Decreased intake torque current to 90A 3. Increased intake supply current to 50A and lower limit to 40A 4. Increased supply current lower limit time from 0.2 to 0.25 seconds

--- src/main/java/frc/robot/util/constants/IntakeConstants.java
+++ src/main/java/frc/robot/util/constants/IntakeConstants.java
@@ -102,17 +102,17 @@ public final class IntakeConstants {
   public static final Voltage INTAKE_ROLLER_VOLTAGE = Volts.of(12);
   public static final double INTAKE_ROLLER_RPM = 6000.0;
   public static final Current INTAKE_ROLLER_TORQUE_CURRENT = Amps.of(90);
-  public static final double INTAKE_ROLLER_TORQUE_CURRENT_MAX_DUTY_CYCLE = 0.8;
+  public static final double INTAKE_ROLLER_TORQUE_CURRENT_MAX_DUTY_CYCLE = 0.9;
   public static final double OUTTAKE_ROLLER_DUTY_CYCLE = -1.0;
   public static final Voltage OUTTAKE_ROLLER_VOLTAGE = Volts.of(-12);
   public static final double OUTTAKE_ROLLER_RPM = -6000.0;
   public static final Current OUTTAKE_ROLLER_TORQUE_CURRENT = Amps.of(-80);
   public static final double OUTTAKE_ROLLER_TORQUE_CURRENT_MAX_DUTY_CYCLE = 0.8;
 
-  public static final Current INTAKE_ROLLER_STATOR_CURRENT_LIMIT = Amps.of(100);
-  public static final Current INTAKE_ROLLER_SUPPLY_CURRENT_LIMIT = Amps.of(40);
-  public static final Current INTAKE_ROLLER_SUPPLY_CURRENT_LOWER_LIMIT = Amps.of(30);
-  public static final Time INTAKE_ROLLER_SUPPLY_CURRENT_LOWER_TIME = Seconds.of(0.2);
+  public static final Current INTAKE_ROLLER_STATOR_CURRENT_LIMIT = Amps.of(90);
+  public static final Current INTAKE_ROLLER_SUPPLY_CURRENT_LIMIT = Amps.of(50);
+  public static final Current INTAKE_ROLLER_SUPPLY_CURRENT_LOWER_LIMIT = Amps.of(40);
+  public static final Time INTAKE_ROLLER_SUPPLY_CURRENT_LOWER_TIME = Seconds.of(0.25);
 
   /* Motor controller configs */
 
